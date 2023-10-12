69c248e172d73d39075e793f732bf4da496219ea
wuchang <chang.wu@shopee.com>
2023-10-12 20:51:57 +0800

    test for git

---
+++ src/main/java/com/shopee/shopeegit/gitlab/IllegalGitLabUrlException.java
@@ -0,0 +1,20 @@
+package com.shopee.shopeegit.gitlab;
+
+public class IllegalGitLabUrlException extends RuntimeException {
+
+    public IllegalGitLabUrlException() {
+        super();
+    }
+
+    public IllegalGitLabUrlException(String msg) {
+        super(msg);
+    }
+
+    public IllegalGitLabUrlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IllegalGitLabUrlException(Throwable cause) {
+        super(cause);
+    }
+}
